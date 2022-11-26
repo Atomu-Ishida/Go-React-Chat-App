2ace7113576ae98400dda1343b34dd64f1468318
Atomu-Ishida <atomu.10.18@icloud.com>
2022-11-26 18:47:12 +0900

    入力・ヘッダー・メッセージの実装

--- frontend/src/components/ChatInput/ChatInput.jsx
+++ frontend/src/components/ChatInput/ChatInput.jsx
@@ -1,2 +1,12 @@
 import React, { Component } from "react";
 import './ChatInput.scss'
+
+class ChatInput extends Component {
+  render() {
+    return (
+      <div className="ChatInput">
+        <input onKeyDown={this.props.send} placeholder="何かメッセージを入力してください 入力したらエンターを押してください"></input>
+      </div>
+    )
+  }
+}
